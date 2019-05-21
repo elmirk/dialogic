8479db303069f1ecc92174ee267aca2a4589708a
elmirk <karimullin@bk.ru>
2019-05-21 22:10:17 +0300

    Update bootstrap and run scripts
    -use docker enviromnent variables for incoming and outgoing number of dialogues;
    -use run_ss7.sh with parameter - DEV or PROD to run in different environments;
    -run_ss7.sh script check if ss7 already running
    -colorized output in run_ss7.sh - just for fun!

--- bootstrap.sh
+++ bootstrap.sh
@@ -65,7 +65,7 @@ fi
 echo -e $Lic >> system.txt
 
 #change TCAP_CONFIG line in config.txt
-Tcap_conf="TCAP_CONFIG 0x0000 ${TCAP_ODLGS_NUM} 0x8000 ${TCAP_IDLGS_NUM} 0x0200 0"
+Tcap_conf="TCAP_CONFIG 0x0000 ${SMSR_TCAP_ODLGS_NUM} 0x8000 ${SMSR_TCAP_IDLGS_NUM} 0x0200 0"
 sed -i "s/^TCAP_CONFIG.*/$Tcap_conf/" ./config.txt
 
 echo -e "finished ok"
--- run_ss7.sh
+++ run_ss7.sh
@@ -1,11 +1,65 @@
 #!/bin/bash
 
-#in prod use detached mode and should set entrypoint in dockerfile
-#docker run -it --log-driver=json-file --log-opt env=DIALOGIC_STAGE --log-opt max-size=10m --log-opt max-file=15 -e DIALOGIC_STAGE=DEV --rm --name=ss7 --ipc="host" --network="host" -v /etc/localtime:/etc/localtime:ro ss7:0.0.0 bash
+#elmir.karimullin@gmail.com
+
+#SCRIPT USAGE:
+#to run ss7 in production use:
+# run_ss7.sh PROD
+#to run ss7 in dev use:
+# run_ss7.sh DEV
+
 
 #when use ELK
 #docker run -it --log-driver gelf --log-opt gelf-address=tcp://localhost:5000 -e DIALOGIC_STAGE=DEV --rm --name=dialogic --ipc="host" --network="host" dialogic:0.0.0 bash
 
 
-#run ss7 container on main prod server castor
-docker run -dit --log-driver=json-file --log-opt env=DIALOGIC_STAGE --log-opt max-size=10m --log-opt max-file=15 -e DIALOGIC_STAGE=PROD -e TCAP_ODLGS_NUM=16000 -e TCAP_IDLGS_NUM=16000 --rm --name=ss7 --ipc="host" --network="host" ss7:0.0.0
+if [ -n "$1" ]
+then
+    if [ $1 == 'DEV' ] || [ $1 == 'PROD' ] 
+    then
+        echo "DIALOGIC_STAGE=$1"
+    else
+        echo "Script usage:"
+        echo -e "\e[32m./run_ss7.sh DEV\e[0m to run ss7 in dev environment"
+        echo -e "\e[32m./run_ss7.sh PROD\e[0m to run ss7 in production environment"
+        exit
+    fi
+else
+    echo "Script usage:"
+    echo -e "\e[32m./run_ss7.sh DEV\e[0m to run ss7 in dev environment"
+    echo -e "\e[32m./run_ss7.sh PROD\e[0m to run ss7 in production environment"
+    exit
+fi
+
+#check last column of docker ps cmd result
+OUT=$(docker ps | awk '{print $NF}' | grep ss7)
+
+if [ $PIPESTATUS -eq 0 ]
+then
+    echo -e "\e[31m!!!Warning!!!Container ss7 exist, use \e[32mdocker ps\e[31m to check if ss7 running!\e[0m"
+    echo -e "\e[31m!!!Warning!!!If running, then use \e[32mdocker stop ss7\e[31m and try again!\e[0m"
+    echo -e "\e[31m!!!Warning!!!script exit now...\e[0m"
+    exit
+fi
+
+#check existance of environment varibles
+if [ -n "$SMSR_TCAP_IDLGS_NUM" ]; then
+  echo 'SMSR_TCAP_IDLGS_NUM = $SMSR_TCAP_IDLGS_NUM'
+else
+    echo -e "\e[31m!!!Warning!!!env variable SMSR_TCAP_IDLGS_NUM not set!!!\e[0m"
+    echo -e "\e[31m!!!Warning!!!Set SMSR_TCAP_IDLGS_NUM with \e[32mexport SMSR_TCAP_IDLGS_NUM=VALUE\e[31m and try again!\e[0m"
+    echo -e "\e[31m!!!Warning!!!script exit now...\e[0m"
+    exit
+fi
+
+if [ -n "$SMSR_TCAP_ODLGS_NUM" ]; then
+    echo 'SMSR_TCAP_ODLGS_NUM = $SMSR_TCAP_ODLGS_NUM'
+else
+    echo -e "\e[31m!!!Warning!!!env variable SMSR_TCAP_ODLGS_NUM not set!!!\e[0m"
+    echo -e "\e[31m!!!Warning!!!Set SMSR_TCAP_ODLGS_NUM with \e[32mexport SMSR_TCAP_ODLGS_NUM=VALUE\e[31m and try again!\e[0m"
+    echo -e "\e[31m!!!Warning!!!script exit now...\e[0m"
+    exit
+fi
+
+#docker run
+docker run -dit --log-driver=json-file --log-opt env=DIALOGIC_STAGE --log-opt max-size=10m --log-opt max-file=15 -e DIALOGIC_STAGE=$1 -e SMSR_TCAP_ODLGS_NUM -e SMSR_TCAP_IDLGS_NUM --rm --name=ss7 --ipc="host" --network="host" ss7:0.0.0
