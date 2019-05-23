10e72076d4ae1790e057da8c31f88eaa0183ad4e
elmirk <karimullin@bk.ru>
2019-05-23 23:32:32 +0300

    minor improvements - remove --rm from docker run - because do not need to remove container when stopped

--- run_ss7.sh
+++ run_ss7.sh
@@ -62,4 +62,4 @@ else
 fi
 
 #docker run
-docker run -dit --log-driver=json-file --log-opt env=DIALOGIC_STAGE --log-opt max-size=10m --log-opt max-file=15 -e DIALOGIC_STAGE=$1 -e SMSR_TCAP_ODLGS_NUM -e SMSR_TCAP_IDLGS_NUM --rm --name=ss7 --ipc="host" --network="host" ss7:0.0.0
+docker run -dit --log-driver=json-file --log-opt env=DIALOGIC_STAGE --log-opt max-size=10m --log-opt max-file=15 -e DIALOGIC_STAGE=$1 -e SMSR_TCAP_ODLGS_NUM -e SMSR_TCAP_IDLGS_NUM --name=ss7 --ipc="host" --network="host" ss7:0.0.0
